4c0d696dcb84cccd17b48c980ff3fe9c72af4068
darkenir <darkenir@naver.com>
2016-08-08 14:20:09 +0900

    Lamp copied

---
+++ Assets/Scripts/Button.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+using System.Collections;
+
+public class Button : MonoBehaviour {
+
+    public int SceneNum;
+
+    // Use this for initialization
+    void Start() {
+
+    }
+
+    // Update is called once per frame
+    void Update() {
+
+    }
+
+    public void OnClick() {
+        SceneManager.LoadScene(SceneNum);
+    }
+}
---
+++ Assets/Scripts/Score.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class Score: MonoBehaviour
+{
+    Text textComponent;
+
+    void ChangeText(string score) {
+        textComponent = GetComponent<Text>();
+        textComponent.text = score;
+    }
+}
